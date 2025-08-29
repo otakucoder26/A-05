b51b4773dc17ff300662e2c6ccfcf5d9082e4acf
otakucoder26 <khanrahmatullah2606@gmail.com>
2025-08-29 18:03:05 +0600

    Call button working

---
+++ script.js
@@ -0,0 +1,54 @@
+// Increment of hearts icon
+const heartChilds = document.getElementsByClassName("heart-child");
+for (let heartChild of heartChilds ){
+    heartChild.addEventListener('click',function(){
+        document.getElementById('heart-parents').innerText++
+    })
+};
+
+// Call Button
+
+
+const callBtns = document.querySelectorAll('.call-btn');
+callBtns.forEach(btn => {
+    btn.addEventListener('click', function (){
+
+        let coin = parseInt(document.getElementById('coin').innerText);
+        let newCoin = coin - 20;
+        document.getElementById('coin').innerText = newCoin;
+        if ( newCoin < 0){
+            document.getElementById('coin').innerText = 0;
+            return alert("X আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০-কয়েন লাগবে'।")
+        }
+        const cards = this.closest('.card');
+        const childrens = cards.querySelectorAll('.child');
+        let allContent = ' '
+        childrens.forEach(child =>{
+            allContent += child.innerText += " ";
+        })
+        alert("Calling"+ allContent +"...");
+
+        const parentDiv = this.closest('.parent-div');
+        const childDivs = parentDiv.querySelectorAll('.call-history');
+        const historyContainer = document.getElementById('history-container');
+
+        const type = childDivs[0].textContent;
+        const name = childDivs[1].textContent;
+
+        let now = new Date();
+        let currentTime = now.toLocaleTimeString(); 
+
+        const historyItem = document.createElement('div');
+        historyItem.innerHTML = `
+        
+        <div style="display:flex; justify-content:space-between; align-items:center;"> <div><div style="font-weight: 600; font-size:14px;">${type}</div> <div style="font-weight: 500;">${name}</div> </div> ${currentTime}</div>
+        
+        
+        `;
+        historyItem.classList.add("bg-[#f2f2f2]","p-3","rounded-md","my-2");
+        historyContainer.appendChild(historyItem);
+
+    });
+
+});
+
